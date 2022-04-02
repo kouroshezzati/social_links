a0dee13cc84be15ccd118668c486a9431daec50a
moslem <moslem.ezati@gmail.com>
2022-04-02 11:20:57 +0430

    feat: Add i18n and material ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import { createTheme } from '@mui/material/styles';
+import rtlPlugin from 'stylis-plugin-rtl';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
+import { prefixer } from 'stylis';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from '@mui/material';
 
-ReactDOM.render(
+const theme = createTheme({
+  direction: 'rtl',
+});
+
+// Create rtl cache
+const cacheRtl = createCache({
+  key: 'muirtl',
+  stylisPlugins: [prefixer, rtlPlugin],
+});
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <CacheProvider value={cacheRtl}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </CacheProvider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
